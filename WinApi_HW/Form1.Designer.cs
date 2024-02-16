a99c34d4672d42473783d89a3426c0da4c5cf2c1
WhiteFishDen <muslinazavtrak@gmail.com>
2024-02-16 20:34:35 +0300

    task 3 ready

--- WinApi_HW/Form1.Designer.cs
+++ WinApi_HW/Form1.Designer.cs
@@ -32,11 +32,14 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(159, 136);
+            this.button1.Location = new System.Drawing.Point(29, 64);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(119, 23);
             this.button1.TabIndex = 0;
@@ -46,7 +49,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(284, 99);
+            this.textBox1.Location = new System.Drawing.Point(199, 28);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
@@ -54,7 +57,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(111, 102);
+            this.label1.Location = new System.Drawing.Point(26, 31);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(167, 13);
             this.label1.TabIndex = 2;
@@ -62,7 +65,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(342, 135);
+            this.button2.Location = new System.Drawing.Point(192, 63);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(107, 24);
             this.button2.TabIndex = 3;
@@ -70,11 +73,43 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(134, 113);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(165, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Play Imperial March";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(418, 63);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(171, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(358, 35);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label2.Size = new System.Drawing.Size(244, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Врспроизвести различные системные звуки : ";
+            // 
             // WinApi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(670, 263);
+            this.ClientSize = new System.Drawing.Size(670, 161);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
@@ -93,6 +128,9 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- WinApi_HW/Form1.cs
+++ WinApi_HW/Form1.cs
@@ -1,23 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WinApi_HW
 {
-    
+
     public partial class WinApi : Form
     {
         
         public WinApi()
         {
             InitializeComponent();
+            comboBox1.Items.AddRange (new string[] {"SimpleBeep", "IconAsterisk", "IconExclamation",
+                                                    "IconHand", "IconQuestion", "Ok"});
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -32,7 +28,10 @@ namespace WinApi_HW
             IntPtr ptr = NativeMethods.FindWindow(null, "WinApiForm");
             NativeMethods.SendMessage(ptr, Convert.ToInt32(NativeMethods.GetWindow_Cmd.WM_SETTEXT), (IntPtr)20, textBox1.Text);
             MessageBox.Show("Заголовок изменен!!!");
+            NativeMethods.Beep(1000,5000);
+
         }
+
 
         private void button2_Click(object sender, EventArgs e)
         {
@@ -46,8 +45,56 @@ namespace WinApi_HW
             {
                 IntPtr ptr = NativeMethods.FindWindow(null,textBox1.Text);
                 NativeMethods.SendMessage(ptr, Convert.ToInt32(NativeMethods.GetWindow_Cmd.WM_CLOSE), (IntPtr)20, "");
-
             }
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() => ImperialMarch());
+            
+        }
+
+         void ImperialMarch()
+         {
+            int a = 750;
+
+            NativeMethods.Beep(392, a);
+            NativeMethods.Beep(392, a);
+            NativeMethods.Beep(392, a);
+            NativeMethods.Beep(311, 563);
+            NativeMethods.Beep(466, 188);
+
+            NativeMethods.Beep(392, a);
+            NativeMethods.Beep(311, 563);
+            NativeMethods.Beep(466, 188);
+            NativeMethods.Beep(392, a * 2);
+
+            NativeMethods.Beep(587, a);
+            NativeMethods.Beep(587, a);
+            NativeMethods.Beep(587, a);
+            NativeMethods.Beep(622, 563);
+            NativeMethods.Beep(466, 188);
+
+            NativeMethods.Beep(369, a);
+            NativeMethods.Beep(311, 563);
+            NativeMethods.Beep(466, 188);
+            NativeMethods.Beep(392, a * 2);
+         }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+                NativeMethods.MessageBeep(NativeMethods.OldBeepTypes.SimpleBeep);
+            if (comboBox1.SelectedIndex == 1)
+                NativeMethods.MessageBeep(NativeMethods.OldBeepTypes.IconAsterisk);
+            if (comboBox1.SelectedIndex == 2)
+                NativeMethods.MessageBeep(NativeMethods.OldBeepTypes.IconExclamation);
+            if (comboBox1.SelectedIndex == 3)
+                NativeMethods.MessageBeep(NativeMethods.OldBeepTypes.IconQuestion);
+            if (comboBox1.SelectedIndex == 4)
+                NativeMethods.MessageBeep(NativeMethods.OldBeepTypes.IconHand);
+            if (comboBox1.SelectedIndex == 5)
+                NativeMethods.MessageBeep(NativeMethods.OldBeepTypes.Ok);
         }
     }
     internal static class NativeMethods
@@ -58,7 +105,20 @@ namespace WinApi_HW
         internal static extern IntPtr FindWindow(string ClassName, string WindowName);
         [DllImport("User32.dll", CharSet = CharSet.Auto)]
         internal static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, string lParam);
-        public enum GetWindow_Cmd : uint
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern bool Beep(int frequency, int duration);
+        [DllImport("user32.dll")]
+  	    internal static extern bool MessageBeep(OldBeepTypes beepType);
+        public enum OldBeepTypes : uint
+  	    {
+  		SimpleBeep = 0xffffffff,
+  		IconAsterisk = 0x00000040,
+  		IconExclamation = 0x00000030,
+ 		IconHand = 0x00000010,
+  		IconQuestion = 0x00000020,
+		Ok = 0x00000000
+ 	    };
+    public enum GetWindow_Cmd : uint
         {
             GW_HWNDFIRST = 0,
             GW_HWNDLAST = 1,
@@ -69,7 +129,8 @@ namespace WinApi_HW
             GW_ENABLEDPOPUP = 6,
             WM_GETTEXT = 0x000D,
             WM_SETTEXT = 0xC,
-            WM_CLOSE = 0x0010
+            WM_CLOSE = 0x0010,
         }
+        
     }
 }
